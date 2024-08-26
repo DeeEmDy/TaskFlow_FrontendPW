d5f8995ecc84f4f47d48b5ae2d2c0676ef9c853f
DixonGM <127709626+DixonGM@users.noreply.github.com>
2024-08-26 15:26:50 -0600

    update
    update the calendar, bug fixes

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,17 +1,15 @@
-// src/pages/Calendar.jsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Calendar, dayjsLocalizer, Views } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import dayjs from 'dayjs';
 import Modal from 'react-modal';
+import '../style/Calendar.css';
 import { showAlert } from '../components/Alert.jsx';
 
 Modal.setAppElement('#root');
 
-const CalendarApp = () => {
-  // Configuración del localizador de fechas
+function App() {
   const localizer = dayjsLocalizer(dayjs);
-  // Estado para almacenar eventos
   const [events, setEvents] = useState([
     {
       start: dayjs('2024-08-08 12:00 AM', 'YYYY-MM-DD hh:mm A').toDate(),
@@ -19,11 +17,9 @@ const CalendarApp = () => {
       title: "Evento de Ejemplo"
     }
   ]);
-  // Estado para la selección de un slot
+
   const [, setSelectedSlot] = useState(null);
-  // Estado para el modal
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  // Estado para el nuevo evento
   const [newEvent, setNewEvent] = useState({
     id_user: '',
     title: '',
@@ -35,16 +31,14 @@ const CalendarApp = () => {
     score: ''
   });
 
-  // Lista de usuarios de ejemplo
   const users = [
     { id: '1', name: 'Usuario 1' },
     { id: '2', name: 'Usuario 2' },
     { id: '3', name: 'Usuario 3' },
   ];
 
-  // Maneja el clic en un slot del calendario
   const handleSlotClick = (slotInfo) => {
-    if (slotInfo.view === Views.MONTH) return; 
+    if (slotInfo.view === Views.MONTH) return;
     setSelectedSlot(slotInfo);
     setModalIsOpen(true);
     setNewEvent({
@@ -54,7 +48,6 @@ const CalendarApp = () => {
     });
   };
 
-  // Maneja el cambio en los campos del formulario
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNewEvent(prevState => ({
@@ -63,7 +56,6 @@ const CalendarApp = () => {
     }));
   };
 
-  // Maneja el envío del formulario
   const handleSubmit = (e) => {
     e.preventDefault();
     setEvents([...events, newEvent]);
@@ -72,28 +64,128 @@ const CalendarApp = () => {
   };
 
   return (
-    <div className="w-full max-w-4xl mx-auto p-4 bg-gray-100 rounded-lg shadow-lg">
+    <div className="calendar-container">
       <Calendar
         localizer={localizer}
         events={events}
         selectable
         onSelectSlot={handleSlotClick}
         onSelectEvent={event => showAlert(event.title)}
-        style={{ height: '81vh' }}
+        style={{ height: '81vh', width: '100%', margin: '0 auto', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
       />
 
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        className="fixed inset-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 w-full max-w-md rounded-lg shadow-lg"
-      >
-        <h2 className="text-xl font-bold mb-4 text-center">Agregar Nuevo Evento</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          {/* Formulario para agregar un evento */}
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="event-modal"  overlayClassName="modal-overlay">
+        <h2 className="modal-title">Agregar Nuevo Evento</h2>
+        <form onSubmit={handleSubmit} className="event-form">
+          <div className="form-group">
+            <label>Título:</label>
+            <input
+              type="text"
+              name="title"
+              value={newEvent.title}
+              onChange={handleInputChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Fecha Tarea Creada:</label>
+            <input
+              type="datetime-local"
+              name="created_task_date"
+              value={dayjs(newEvent.created_task_date).format('YYYY-MM-DDTHH:mm')}
+              onChange={handleInputChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Fecha de Tarea Expirada:</label>
+            <input
+              type="datetime-local"
+              name="expiration_task_date"
+              value={dayjs(newEvent.expiration_task_date).format('YYYY-MM-DDTHH:mm')}
+              onChange={handleInputChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Descripción:</label>
+            <textarea
+              name="description_task"
+              value={newEvent.description_task}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Usuario:</label>
+            <select
+              name="id_user"
+              value={newEvent.id_user}
+              onChange={handleInputChange}
+              required
+              className="form-input"
+            >
+              <option value="">Seleccione un usuario</option>
+              {users.map(user => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </div>
+  
+          <div className="form-group">
+            <label>Progreso de Tarea (%):</label>
+            <input
+              type="range"
+              name="progress_task"
+              value={newEvent.progress_task || 0}
+              onChange={handleInputChange}
+              min="0"
+              max="100"
+              required
+              className="form-input"
+              disabled
+            />
+            <div>{newEvent.progress_task || 0}%</div>
+          </div>
+        
+          <div className="form-group">
+            <label>Fecha Finalización de Tarea:</label>
+            <input
+              type="date"
+              name="finalization_task_date"
+              value={newEvent.finalization_task_date}
+              onChange={handleInputChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label>Puntaje:</label>
+            <input
+              type="number"
+              name="score"
+              value={newEvent.score}
+              onChange={handleInputChange}
+              min="0"
+              max="100"
+              disabled
+              className="form-input"
+            />
+          </div>
+          <div className="form-buttons">
+            <button type="submit" className="submit-button">Agregar Evento</button>
+            <button type="button" onClick={() => setModalIsOpen(false)} className="cancel-button">Cancelar</button>
+          </div>
         </form>
       </Modal>
     </div>
   );
-};
+}
 
-export default CalendarApp;
+export default App;
