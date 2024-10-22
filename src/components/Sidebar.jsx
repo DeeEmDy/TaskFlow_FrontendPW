011c0d52746ef7527e1e708f1252a5d5d7e22e6b
DixonGM <127709626+DixonGM@users.noreply.github.com>
2024-10-22 11:32:18 -0600

    creation of profile view
    creation of profile view and profile icon section.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,87 @@
+import { useNavigate } from "react-router-dom"; 
 import { useState, useEffect } from "react";
 import "../style/Sidebar.css";
 import eventoIcon from "../imagenes/Evento.png"; 
 import calendarIcon from "../imagenes/calendar.png";
 import dashboardIcon from "../imagenes/dashboard1.png";
 import informacionIcon from "../imagenes/informacion.png";
-//import sesionIcon from "../imagenes/cerrar-sesion.png";
 import perfilIcon from "../imagenes/perfil.png";
 import bar1Icon from "../imagenes/bar1.png";
 import inicioIcon from "../imagenes/casa.png";
 import logrosIcon from "../imagenes/logros.png";
-import { FaHome, FaCalendarAlt, FaSignOutAlt, FaCalendarCheck, FaTrophy } from 'react-icons/fa';
+import { FaSignOutAlt } from 'react-icons/fa';
+import { FaCamera, FaTrashAlt } from 'react-icons/fa';
 
 function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [profilePic, setProfilePic] = useState(perfilIcon);
+  const [newName, setNewName] = useState("Dixon Gongora");
+  const [newLastName, setNewLastName] = useState("");
+  const navigate = useNavigate(); // Hook para redirigir
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
+    setIsDropdownOpen(false); // Cierra el dropdown
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    setIsDropdownOpen(false); // Cierra el dropdown
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    
+  };
+
+  
   const closeSidebar = (e) => {
     if (!e.target.closest('.sidebar') && isOpen) {
       setIsOpen(false);
     }
+  };
+
+   // Funciones para redirigir a diferentes rutas
+   const handleProgresoTareas = () => {
+    navigate("/dashBoard"); 
+    setIsDropdownOpen(false); 
+  };
+
+  const handleOtros = () => {
+    navigate("/otros");
+    setIsDropdownOpen(false); 
+  };
+
+  const handleCerrarSesion = () => {
+    navigate("/"); 
+    setIsDropdownOpen(false); 
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setProfilePic(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removeProfilePic = () => {
+    setProfilePic(perfilIcon);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+     
+    closeModal();
   };
 
   useEffect(() => {
@@ -37,22 +97,36 @@ function Sidebar() {
         <div className="menu-btn" onClick={toggleSidebar}>
           <img src={bar1Icon} alt="Bar" style={{ width: '25px', height: '25px' }} />
         </div>
-        <div className="head">
-          <a href="#">
+
+        {/* Perfil con Dropdown */}
+        <div className="relative">
+          <div className="head" onClick={toggleDropdown}>
             <div className="user-img" title="Perfil">
-              <img src={perfilIcon} alt="Perfil" style={{ width: '45px', height: '45px' }} />
+              <img src={profilePic} alt="Perfil" style={{ width: '45px', height: '45px' }} />
             </div>
-          </a>
-          <div className="user-details">
-            <p className="title">Task Flow</p>
-            <a href="#">Dixon Gongora</a>
+            <div className="user-details">
+              <p className="title">Task Flow</p>
+              <a href="#">{newName}</a>
+            </div>
           </div>
+        
+             <div className={`profile-dropdown ${isDropdownOpen ? 'open' : ''}`}>
+              <ul>
+                <li className="p-2 hover:bg-gray-200 cursor-pointer" onClick={openModal}>Perfil</li>
+                <li className="p-2 hover:bg-gray-200 cursor-pointer" onClick={handleProgresoTareas}>Progreso de tareas</li>
+                <li className="p-2 hover:bg-gray-200 cursor-pointer" onClick={handleOtros}>Otros</li>
+                <li className="p-2 hover:bg-gray-200 cursor-pointer" onClick={handleCerrarSesion}>Cerrar Sesión</li>
+              </ul>
+            </div>
+        
         </div>
+
+        {/* Otras opciones */}
         <div className="nav">
           <div className="menu">
             <p className="title">Home</p>
             <ul>
-            <li>
+              <li>
                 <a href="/homePage" title="Inicio">
                   <img src={inicioIcon} alt="Inicio" style={{ width: '25px', height: '25px' }} />
                   <span className="text">Inicio</span>
@@ -84,8 +158,9 @@ function Sidebar() {
               </li>
             </ul>
           </div>
-          
         </div>
+
+
         <div className="menu">
           <p className="title">Account</p>
           <ul>
@@ -103,6 +178,82 @@ function Sidebar() {
             </li>
           </ul>
         </div>
+
+        {/* Modal para editar el perfil */}
+        {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
+          <div className="modal-container bg-white p-6 rounded shadow-lg w-96">
+            <div className="mb-4">
+              <label className="block text-gray-700">Foto de perfil:</label>
+              <div className="flex items-center space-x-4">
+                <img
+                  src={profilePic}
+                  alt="Foto de perfil"
+                  className="w-16 h-16 rounded-full object-cover"
+                />
+                <div>
+                  <label className="text-blue-500 cursor-pointer flex items-center">
+                    <FaCamera className="mr-2" />
+                    Cambiar
+                    <input type="file" onChange={handleFileChange} className="hidden" />
+                  </label>
+                  <button
+                    type="button"
+                    onClick={removeProfilePic}
+                    className="text-red-500 flex items-center mt-2"
+                  >
+                    <FaTrashAlt className="mr-2" />
+                    Eliminar
+                  </button>
+                </div>
+              </div>
+            </div>
+            <br />
+            <h2 className="text-xl font-bold mb-4">Editar Perfil</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label className="block text-gray-700">Nombre:</label>
+                <input
+                  type="text"
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Apellido:</label>
+                <input
+                  type="text"
+                  value={newLastName}
+                  onChange={(e) => setNewLastName(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Correo electrónico:</label>
+                <input
+                  type="email"
+                  value="johndoe@example.com"
+                  disabled
+                  className="w-full p-2 border border-gray-300 bg-gray-100 rounded"
+                />
+              </div>
+              <div className="text-right">
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  className="mr-2 px-4 py-2 bg-gray-300 text-gray-700 rounded"
+                >
+                  Cancelar
+                </button>
+                <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
+                  Guardar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
       </div>
     </div>
   );
