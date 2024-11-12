c3a2fe2e07b3deccea56d7aaff9cd554c0076f2a
DeeEmDy <dy-lanarias@hotmail.comcls>
2024-11-11 18:24:20 -0600

    Revert "adjust routes config to protected paths"
    This reverts commit aab687cd0f3335debfdb66925b7dbc9b6aac92f9.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AppRouter from './routes/AppRouter';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './components/App';
-import Calendar from './pages/Calendar';
-import HomePage from './pages/HomePage';
-import Layout from './components/Layout';
-import DashBoard from './components/DashBoard';
-import ForgotPassword from './components/Forgot-password';
-import ProtectedRoute from './components/ProtectedRoute'; // Importa el ProtectedRoute
 
 // 1. Creación del cliente de React Query
 const queryClient = new QueryClient();
@@ -20,49 +14,9 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          {/* Ruta de App sin Sidebar */}
-          <Route path="/" element={<App />} />
-          <Route path="/forgotPassword" element={<ForgotPassword />} />
-
-          {/* Ruta de HomePage con Sidebar envuelta en ProtectedRoute */}
-          <Route 
-            path="/homePage" 
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <HomePage />
-                </Layout>
-              </ProtectedRoute>
-            } 
-          />
-
-          {/* Ruta de Calendar con Sidebar envuelta en ProtectedRoute */}
-          <Route 
-            path="/calendar" 
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Calendar />
-                </Layout>
-              </ProtectedRoute>
-            } 
-          />
-
-          {/* Ruta de DashBoard con Sidebar envuelta en ProtectedRoute */}
-          <Route 
-            path="/dashBoard" 
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <DashBoard />
-                </Layout>
-              </ProtectedRoute>
-            } 
-          />
-        </Routes>
+        <AppRouter /> {/* Renderiza las rutas desde el archivo dedicado */}
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} /> {/* Herramienta para depuración */}
+      <ReactQueryDevtools initialIsOpen={false} /> {/* Herramienta para depuración de React Query */}
     </QueryClientProvider>
   </React.StrictMode>
 );
